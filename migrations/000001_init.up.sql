3f89f945243bf0bfad70871c185d2bb795a0b25a
adarien <aalian.darien@gmail.com>
2023-04-27 15:05:01 +0700

    update migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -3,3 +3,14 @@ CREATE TABLE users
     tg_user_id BIGINT PRIMARY KEY,
     lang       VARCHAR(5) NOT NULL DEFAULT 'en'
 );
+
+CREATE TABLE playlist_info
+(
+    id             SERIAL PRIMARY KEY,
+    playlist_title VARCHAR(255) NOT NULL,
+    video_id       VARCHAR(255) NOT NULL,
+    video_title    VARCHAR(255) NOT NULL,
+    owner_id       VARCHAR(255) NOT NULL,
+    owner_title    VARCHAR(255) NOT NULL,
+    playlist_id    VARCHAR(255) NOT NULL
+);
